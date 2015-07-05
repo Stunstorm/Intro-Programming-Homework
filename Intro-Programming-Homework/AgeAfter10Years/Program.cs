37755a0f5ad7cd8a5c6fac60db755e5334067333
StunStorm <stanislav.zaykov@gmail.com>
2015-07-05 04:17:02 +0300

    Initial commit, adding files.

---
+++ Intro-Programming-Homework/AgeAfter10Years/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeAfter10Years
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your birthday");
+            DateTime Birthday = DateTime.Parse(Console.ReadLine());
+            DateTime today = DateTime.Now;
+            int age = today.Year - Birthday.Year;
+            if ((Birthday.Month <= today.Month && Birthday.Day <= today.Day) ^ (Birthday.Month < today.Month && Birthday.Day > today.Day))
+            {
+                Console.WriteLine("You are {0} years old.", age);
+                Console.WriteLine("After 10 years you will be {0} years old", age + 10);
+            }
+            else
+            {
+                Console.WriteLine("You are {0} years old.", age - 1);
+                Console.WriteLine("After 10 years you will be {0} years old", age + 9);
+            }
+        }
+    }
+}
